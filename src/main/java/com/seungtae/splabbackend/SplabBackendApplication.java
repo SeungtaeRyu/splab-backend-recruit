5294d3e1d993c165a10edc310776a5cbb8fd73bd
SeungtaeRyu <dbtmdxo1992@naver.com>
2024-05-05 22:18:50 +0900

    init

---
+++ src/main/java/com/seungtae/splabbackend/SplabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seungtae.splabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungtae/splabbackend/SplabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungtae.splabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
