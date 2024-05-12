9ed74e91d53eccb4e7ac3a33f6b172001a1bd119
SeungtaeRyu <dbtmdxo1992@naver.com>
2024-05-12 20:36:31 +0900

    user entity 작성

--- src/main/java/com/seungtae/splabbackend/domain/user/entity/UserEntity.java
+++ src/main/java/com/seungtae/splabbackend/domain/user/entity/UserEntity.java
@@ -2,8 +2,13 @@ package com.seungtae.splabbackend.domain.user.entity;
 
 
 import com.seungtae.splabbackend.common.entity.BaseEntity;
+import com.seungtae.splabbackend.domain.group.entity.GroupEntity;
+import com.seungtae.splabbackend.domain.invite.entity.InviteEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -12,10 +17,12 @@ public class UserEntity extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long userId;
 
-	private String name;
-	private String phone;
-	private String email;
-	private InvitaionStatus status;
+	private String userName;
+	private String userPhone;
+	private String userEmail;
+
+	@OneToMany(mappedBy = "member")
+	private List<InviteEntity> invitedGroups = new ArrayList<>();
 }
